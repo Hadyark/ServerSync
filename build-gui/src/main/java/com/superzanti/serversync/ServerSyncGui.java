4db2d5cf8760036c0d540bb43f4e46230b76a7c4
Hadyark <32941087+Hadyark@users.noreply.github.com>
2024-12-21 13:40:42 +0100

    Move project to composite build

---
+++ build-gui/src/main/java/com/superzanti/serversync/ServerSyncGui.java
@@ -0,0 +1,10 @@
+package com.superzanti.serversync;
+
+/**
+ * Wrapper for executables to call when starting in server mode
+ */
+public class ServerSyncGui {
+    public static void main(String[] args) {
+        ServerSync.main(new String[]{});
+    }
+}
